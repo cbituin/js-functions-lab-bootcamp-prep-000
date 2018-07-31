28e4a3b15a91c71dfeec7daaec37941b8bb1bbcc
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-07-31 13:30:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 
+<<<<<<< HEAD
 function happyHolidaysTo(name) {
   return 'Happy holidays, ' + name + '!';
 }
@@ -13,4 +14,17 @@ function happyHolidayTo(holiday, name){
 
 function holidayCountdown(holiday, days){
   return "It's " + days + " days until " + holiday + "!";
+=======
+function happyHolidaysTo(name){
+  //return 'Happy Holidays, ${name}!';
+  return "Happy holidays, ${name}!"
+}
+
+function happyHolidayTo(holiday, name){
+  return 'Happy ${holiday}, ${name}!';
+}
+
+function holidayCountdown(days, holiday){
+  return 'It\'s ${days} days until ${holiday}!';
+>>>>>>> 5a1899463a64a5adc71135eb407e24d509b8174f
 }
